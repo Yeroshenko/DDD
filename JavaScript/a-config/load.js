3d6ea600b07e0a6bb078436040665921203f16bf
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2022-10-09 12:26:06 +0300

    feat: add config file

---
+++ JavaScript/a-config/load.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const fs = require('node:fs').promises;
+const vm = require('node:vm');
+const config = require('./config.js')
+
+const RUN_OPTIONS = config.sandbox
+
+module.exports = async (filePath, sandbox) => {
+  const src = await fs.readFile(filePath, 'utf8');
+  const code = `'use strict';\n${src}`;
+  const script = new vm.Script(code);
+  console.log({ script });
+  const context = vm.createContext(Object.freeze({ ...sandbox }));
+  const exported = script.runInContext(context, RUN_OPTIONS);
+  console.log({ exported });
+  return exported;
+};
